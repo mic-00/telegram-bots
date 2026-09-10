565eacf1399913651e00844f1a6761b4f3c00018
mic-00 <michele.masetto@gmail.com>
2026-09-10 15:53:54 +0200

    add HelloWorldBot.java

---
+++ src/main/java/it/michelemasetto/telegram/bots/helloworld/HelloWorldBot.java
@@ -0,0 +1,31 @@
+package it.michelemasetto.telegram.bots.helloworld;
+
+import it.michelemasetto.telegram.bots.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+public class HelloWorldBot extends TelegramBot {
+
+    private static final String HELLO_WORLD_BOT_TOKEN = "HELLO_WORLD_BOT_TOKEN";
+
+    public HelloWorldBot() {
+        super(System.getenv(HELLO_WORLD_BOT_TOKEN));
+    }
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage()) {
+            Long chatId = update.getMessage().getChatId();
+            SendMessage sendMessage = SendMessage.builder().chatId(chatId).text("Hello World!").build();
+
+            try {
+                telegramClient.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                log.error("An error occurred during client execution", e);
+            }
+        }
+    }
+}
