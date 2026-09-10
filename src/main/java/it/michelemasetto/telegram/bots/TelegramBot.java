ed187327e708e07ea1ab52811719d095e05969fe
mic-00 <michele.masetto@gmail.com>
2026-09-10 17:06:14 +0200

    refactor

--- src/main/java/it/michelemasetto/telegram/bots/TelegramBot.java
+++ src/main/java/it/michelemasetto/telegram/bots/TelegramBot.java
@@ -1,31 +1,14 @@
 package it.michelemasetto.telegram.bots;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.util.DefaultLongPollingUpdateConsumer;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 @Slf4j
 @Getter
+@AllArgsConstructor
 public abstract class TelegramBot extends DefaultLongPollingUpdateConsumer {
 
     protected final String botToken;
-
-    protected final TelegramClient telegramClient;
-
-    public TelegramBot(String botToken) {
-        this.botToken = botToken;
-        this.telegramClient = new OkHttpTelegramClient(botToken);
-    }
-
-    public final void registerTo(TelegramBotsLongPollingApplication app) {
-        try {
-            app.registerBot(botToken, this);
-        } catch (TelegramApiException e) {
-            log.error("An error occurred during bot registration", e);
-        }
-    }
 }
--- src/main/java/it/michelemasetto/telegram/bots/helloworld/HelloWorldBot.java
+++ src/main/java/it/michelemasetto/telegram/bots/helloworld/HelloWorldBot.java
@@ -2,17 +2,22 @@ package it.michelemasetto.telegram.bots.helloworld;
 
 import it.michelemasetto.telegram.bots.TelegramBot;
 import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 @Slf4j
 public class HelloWorldBot extends TelegramBot {
 
     private static final String HELLO_WORLD_BOT_TOKEN = "HELLO_WORLD_BOT_TOKEN";
 
+    private final TelegramClient telegramClient;
+
     public HelloWorldBot() {
         super(System.getenv(HELLO_WORLD_BOT_TOKEN));
+        telegramClient = new OkHttpTelegramClient(botToken);
     }
 
     @Override
