a9c76a19cd680b3e68184d22c30c3d25aba660b6
mic-00 <michele.masetto@gmail.com>
2026-09-12 03:02:33 +0200

    refactor

--- src/main/java/it/michelemasetto/telegram/bots/helloworld/HelloWorldBot.java
+++ src/main/java/it/michelemasetto/telegram/bots/helloworld/HelloWorldBot.java
@@ -1,36 +1,24 @@
 package it.michelemasetto.telegram.bots.helloworld;
 
 import it.michelemasetto.telegram.bots.TelegramBot;
-import lombok.extern.slf4j.Slf4j;
-import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-@Slf4j
 public class HelloWorldBot extends TelegramBot {
 
     private static final String HELLO_WORLD_BOT_TOKEN = "HELLO_WORLD_BOT_TOKEN";
 
-    private final TelegramClient telegramClient;
-
     public HelloWorldBot() {
         super(System.getenv(HELLO_WORLD_BOT_TOKEN));
-        telegramClient = new OkHttpTelegramClient(botToken);
     }
 
     @Override
-    public void consume(Update update) {
+    protected void doConsume(Update update) throws Exception {
         if (update.hasMessage()) {
             Long chatId = update.getMessage().getChatId();
             SendMessage sendMessage = SendMessage.builder().chatId(chatId).text("Hello World!").build();
 
-            try {
-                telegramClient.execute(sendMessage);
-            } catch (TelegramApiException e) {
-                log.error("An error occurred during client execution", e);
-            }
+            getTelegramClient().execute(sendMessage);
         }
     }
 }
